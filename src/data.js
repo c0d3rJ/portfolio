ce0b19a51e4e25e338e29f39ca506268b06916ee
c0d3rJ <c0d3rJJ@protonmail.com>
2022-02-01 13:15:49 -0400

    v1.8.1 updated data.js with new repos and stats

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export const projects = [
     },
     {
         title: "Portfolio Site",
-        subtitle: "Another Hosting of my Portfolio Site",
+        subtitle: "Another Hosting of my Portfolio Site DEMO",
         buildWith: "React",
         description:
             "Built with ReactJS and Tailwind CSS this is a Portfolio Site that can be used by a developer to share their work.",
@@ -21,14 +21,36 @@ export const projects = [
         source: "https://github.com/c0d3rJ/portfolio",
         techs: [{name: 'react'},{name: 'tailwind css'},{name: 'html'}]
     },
+    {
+        title: "Clothing e-Store",
+        subtitle: "Github link to crwn-clothing e-store site",
+        buildWith: "React, SCSS",
+        description:
+          "Built with ReactJS and SCSS this is a e-Store Built to develop my skills and knowledge of React.",
+        image: "",
+        live: "",
+        source: "https://github.com/c0d3rJ/crwn-clothing",
+        techs: [{name: 'react'},{name: 'scss'},{name: 'html'},{name: 'firebase'}]
+    },
+    {
+        title: "Financial Tracker",
+        subtitle: "Github link to Financial Tracker Repo",
+        buildWith: "React, SCSS",
+        description:
+          "Built with NodeJS, ReactJS and TailwindCSS this is a simple financial tracker app made with friends and love over the Christmas Vacation ",
+        image: "",
+        live: "",
+        source: "https://github.com/JChris246/financial_tracker",
+        techs: [{name: 'react'},{name: 'TailwindCSS'},{name: 'NodeJS'}]
+    },
 ];
 
 export const skills = [
     {name: "html", level: 90},
     {name: "css", level: 73},
     {name: "react", level: 40},
-    {name: 'javascript', level: 32},
-    {name: '.net', level: 24},
+    {name: 'javascript', level: 24},
+    {name: '.net', level: 20},
     {name: "java", level: 35},
     {name: 'mysql', level: 20}
 ];
@@ -37,7 +59,7 @@ export const skills2 = [
     {name: "html", level: 90},
     {name: "css", level: 73},
     {name: "react", level: 40},
-    {name: 'javascript', level: 32},
+    {name: 'javascript', level: 24},
     {name: "c++", level: 40},
     {name: "java", level: 35},
     {name: '.net', level: 24},
