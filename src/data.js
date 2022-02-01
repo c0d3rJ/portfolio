74bef0313541a36b30c07287792c0af8b6f12ee2
c0d3rJ <c0d3rJJ@protonmail.com>
2022-02-01 13:15:49 -0400

    v1.8.2 updated data.js with new repos and stats

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export const projects = [
     },
     {
         title: "Portfolio Site",
-        subtitle: "Another Hosting of my Portfolio Site",
+        subtitle: "Another Hosting of my Portfolio Site DEMO",
         buildWith: "React",
         description:
             "Built with ReactJS and Tailwind CSS this is a Portfolio Site that can be used by a developer to share their work.",
@@ -21,14 +21,69 @@ export const projects = [
         source: "https://github.com/c0d3rJ/portfolio",
         techs: [{name: 'react'},{name: 'tailwind css'},{name: 'html'}]
     },
+    {
+        title: "Clothing e-Store",
+        subtitle: "Github link to crwn-clothing e-store site",
+        buildWith: "React, SCSS",
+        description:
+          "Built with ReactJS and SCSS this is a e-Store Built to develop my skills and knowledge of React.",
+        image: "",
+        live: "",
+        source: "https://github.com/c0d3rJ/crwn-clothing",
+        techs: [{name: 'react'},{name: 'scss'},{name: 'html'},{name: 'firebase'}]
+    },
+    {
+        title: "Financial Tracker",
+        subtitle: "Github link to Financial Tracker Repo",
+        buildWith: "React, SCSS",
+        description:
+          "Built with NodeJS, ReactJS and TailwindCSS this is a simple financial tracker app made with friends and love over the Christmas Vacation ",
+        image: "",
+        live: "",
+        source: "https://github.com/JChris246/financial_tracker",
+        techs: [{name: 'react'},{name: 'TailwindCSS'},{name: 'NodeJS'}]
+    },
+    {
+        title: ".NET CRUD Datatables Demo",
+        subtitle: "Github link to Datatables Example Repo",
+        buildWith: "C#, jquery",
+        description:
+          "Made with .Net and jquery Datatables, this project is a simple CRUD app done to learn the fundamentals of C# and .Net",
+        image: "",
+        live: "",
+        source: "https://github.com/c0d3rJ/ASPNetMVCDatatablesExample",
+        techs: [{name: '.Net Core'},{name: 'RazorPages'},{name: 'jQuery Datatables'}]
+    },
+    {
+        title: "CitizenDB Springboot API Server",
+        subtitle: "A Spring Boot Driven Server for CitizenDB Project",
+        buildWith: "Java, Spring Boot",
+        description:
+          "Made with Java Spring Boot , this project is a simple API Server done to learn the fundamentals of Java Spring Boot",
+        image: "",
+        live: "",
+        source: "https://github.com/c0d3rJ/citizendb-springboot-api-server/",
+        techs: [{name: 'Java'},{name: 'Spring Boot'},{name: 'mysql'}]
+    },
+    {
+        title: "CitizenDB React Client",
+        subtitle: "CitizenDB - A Simple Client app for the CitizenDB Project",
+        buildWith: "React, axios, Material UI",
+        description:
+          "Built with ReactJS, Axios, MUI-DataGrid and React-Tables, this a simple frontend to the CitizenDB Server Project",
+        image: "",
+        live: "",
+        source: "https://github.com/c0d3rJ/citizendb-react-client",
+        techs: [{name: 'react'},{name: 'axios'},{name: 'Material UI'}]
+    },
 ];
 
 export const skills = [
     {name: "html", level: 90},
     {name: "css", level: 73},
     {name: "react", level: 40},
-    {name: 'javascript', level: 32},
-    {name: '.net', level: 24},
+    {name: 'javascript', level: 24},
+    {name: '.net', level: 20},
     {name: "java", level: 35},
     {name: 'mysql', level: 20}
 ];
@@ -37,7 +92,7 @@ export const skills2 = [
     {name: "html", level: 90},
     {name: "css", level: 73},
     {name: "react", level: 40},
-    {name: 'javascript', level: 32},
+    {name: 'javascript', level: 24},
     {name: "c++", level: 40},
     {name: "java", level: 35},
     {name: '.net', level: 24},
